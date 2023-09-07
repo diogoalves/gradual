15dd6d6d0da64a42329a893821b18fd488727cea
Diogo Alves <diogo.alves@gmail.com>
2023-09-07 10:31:50 +0000

    result of day2

---
+++ gradual/engine.py
@@ -0,0 +1,209 @@
+import math 
+import numpy as np
+
+class Value:
+  ''' 
+      Base class used to define an auto-diff expression graph.
+      It represents a single scalar value.
+  '''
+
+  def __init__(self, data, _children=(), _op=(), label=''):
+    self.data = data
+    self.grad = 0
+    self.label = label
+
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+
+  def __add__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), '+')
+
+    def _backward():
+      self.grad += out.grad
+      other.grad += out.grad
+    out._backward = _backward
+
+    return out
+
+  def __mul__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data * other.data, (self, other), '*')
+
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
+
+    return out
+
+  def __pow__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data ** other.data, (self,), f'**{other}')
+
+    def _backward():
+      self.grad += ((other.data * (self.data ** (other.data) ))/ self.data) * out.grad
+      try:
+        other.grad += ((self.data ** other.data) * math.log(self.data)) * out.grad
+      except:
+        other.grad += 0
+    out._backward = _backward
+
+    return out
+
+  def relu(self):
+    out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+    def _backward():
+      self.grad += (out.data > 0) *  out.grad
+    out._backward = _backward
+
+    return out
+  
+  def tanh(self):
+    e2x = math.e ** (2 * self.data)
+    out = Value( (e2x - 1)/(e2x + 1), (self,), 'ReLU')
+
+    def _backward():
+      self.grad += (1 - out.data**2) * out.grad
+    out._backward = _backward
+
+  # composite operator functions
+  def __neg__(self): return self * -1
+  def __sub__(self, other): return self + (-other)
+  def __truediv__(self, other): return self * (other ** -1)
+
+  # reverse operator functions
+  def __radd__(self, other): return self + other # other + self
+  def __rsub__(self, other): return other + (-self) # other - self
+  def __rmul__(self, other): return self * other # other * self
+  def __rtruediv__(self, other): return other * (self ** - 1) # other / self
+
+  # printable representation
+  def __repr__(self): return f'Value(data={self.data}, grad={self.grad}, label={self.label})'
+  
+  def backward(self):
+    # topological order all of the children in the graph
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    # go one variable at a time and apply the chain rule to get its gradient
+    self.grad = 1
+    for v in reversed(topo):
+      v._backward()    
+
+
+
+
+
+class Tensor:
+  ''' 
+      Tensor class used to define an auto-diff expression graph.
+  '''
+
+  def __init__(self, data, _children=(), _op=(), label=''):
+    self.data = np.array(data)
+    self.grad = np.zeros(self.data.shape)
+    self.label = label
+
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+
+  def __add__(self, other):
+    if not isinstance(other, Tensor):
+      if isinstance(other, (int, float)):
+        other = Tensor([other])
+      else:
+        other = Tensor(other)
+    out = Tensor(self.data + other.data, (self, other), '+')
+
+    def _backward():
+      self.grad += out.grad
+      other.grad += out.grad
+    out._backward = _backward
+
+    return out
+
+  def __mul__(self, other):
+    if not isinstance(other, Tensor):
+      if isinstance(other, (int, float)):
+        other = Tensor([other])
+      else:
+        other = Tensor(other)
+    out = Tensor(self.data * other.data, (self, other), '*')
+
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
+
+    return out
+
+  def __pow__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data ** other.data, (self,), f'**{other}')
+
+    def _backward():
+      self.grad += ((other.data * (self.data ** (other.data) ))/ self.data) * out.grad
+      try:
+        with np.errstate(divide = 'ignore'):
+          other.grad += ((self.data ** other.data) * np.log(self.data)) * out.grad
+      except:
+        other.grad += 0
+    out._backward = _backward
+
+    return out
+
+  def relu(self):
+    out = Tensor(np.maximum(0, self.data), (self,), 'ReLU')
+
+    def _backward():
+      self.grad += (out.data > 0) *  out.grad
+    out._backward = _backward
+
+    return out
+
+  # composite operator functions
+  def __neg__(self): return self * [-1]
+  def __sub__(self, other): return self + (-other)
+  def __truediv__(self, other):
+    if not isinstance(other, Tensor):
+      if isinstance(other, (int, float)):
+        other = Tensor([other])
+      else:
+        other = Tensor(other)
+    return self * (other ** -1)
+
+  # reverse operator functions
+  def __radd__(self, other): return self + other # other + self
+  def __rsub__(self, other): return other + (-self) # other - self
+  def __rmul__(self, other): return self * other # other * self
+  def __rtruediv__(self, other): return other * (self ** [- 1]) # other / self
+
+  def __repr__(self): return f'Value(data={self.data}, grad={self.grad}, label={self.label})'
+
+  def backward(self):
+    # topological order all of the children in the graph
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+
+    # go one variable at a time and apply the chain rule to get its gradient
+    self.grad = np.ones(self.data.shape)
+    for v in reversed(topo):
+      v._backward()    
---
+++ gradual/util.py
@@ -0,0 +1,46 @@
+import numpy as np
+from graphviz import Digraph
+from .engine import Value, Tensor
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        if isinstance(n, Value):
+            data = f'{n.data:.4f}'
+            grad = f'{n.grad:.4f}'
+        elif isinstance(n, Tensor):
+            data = np.array2string(n.data, precision=4, separator=",")
+            grad = np.array2string(n.grad, precision=4, separator=",")
+        else:
+            print(n)
+            print(type(n))
+            data = 'Unknow'
+            grad = 'Unknow'
+        node_label = f'{{ {n.label} | data {data} | grad {grad} }}'
+        dot.node(name=str(id(n)), label = node_label, shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
